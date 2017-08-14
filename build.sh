4fcb3f92646191d92eb84087c74f240102caef21
opera443399 <pc@pcswo.com>
2017-08-14 10:16:51 +0800

    chore(whoami): tag the latest with version tag

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+# 2017/8/14
+
+image_name='opera443399/whoami'
+image_ver=$(grep 'version' app.go |grep -Eo '[0-9].[0-9]')
 CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o whoamI
-docker build -t opera443399/whoami .
+docker build -t ${image_name} .
+docker tag ${image_name} ${image_name}:${image_ver}
+docker images |grep "${image_name}"
