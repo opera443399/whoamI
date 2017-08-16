f315b1d51511352a75c64f98924e47c10edd1411
opera443399 <pc@pcswo.com>
2017-08-16 11:05:41 +0800

    chore(whoami): fomat and stadout/err enhanced.

--- app.go
+++ app.go
@@ -6,41 +6,41 @@
  * go install app.go
  * CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o whoamI
  *
- * pc@2017/8/14
+ * pc@2017/8/16
  *
 */
 
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"fmt"
-	"log"
-	"sync"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "log"
+    "sync"
 
-	"net/http"
-	"net/url"
-	"os"
-	"time"
+    "net/http"
+    "net/url"
+    "os"
+    "time"
 )
 
 var port string
 
 func init() {
-	flag.StringVar(&port, "port", "80", "listen the port as given.")
+    flag.StringVar(&port, "port", "80", "listen the port as given.")
 }
 
 func main() {
-	flag.Parse()
-        // url: index
-	http.HandleFunc("/", index)
-        // url: others
-	http.HandleFunc("/api", api)
-	http.HandleFunc("/health", healthHandler)
-	http.HandleFunc("/test", testHandler)
-	log.Println("Listening on port *:" + port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+    flag.Parse()
+    // url: index
+    http.HandleFunc("/", index)
+    // url: others
+    http.HandleFunc("/api", api)
+    http.HandleFunc("/health", healthHandler)
+    http.HandleFunc("/test", testHandler)
+    fmt.Println("Listening on port *:" + port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 /*
@@ -49,10 +49,10 @@ func main() {
 */
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
-        body := "[test] Hello World\n"
-        w.Header().Set("Connection", "keep-alive")
-        w.Header().Set("Content-Type", "text/plain")
-        fmt.Fprint(w, body)
+    body := "[test] Hello World\n"
+    w.Header().Set("Connection", "keep-alive")
+    w.Header().Set("Content-Type", "text/plain")
+    fmt.Fprint(w, body)
 }
 
 
@@ -63,36 +63,37 @@ func testHandler(w http.ResponseWriter, r *http.Request) {
 */
 
 func index(w http.ResponseWriter, req *http.Request) {
-        w.Header().Set("Connection", "close")
 
-	u, _ := url.Parse(req.URL.String())
-	queryParams := u.Query()
-	wait := queryParams.Get("wait")
-	if len(wait) > 0 {
-		duration, err := time.ParseDuration(wait)
-		if err == nil {
-                        log.Println("request will be delayed for :", duration)
-			time.Sleep(duration)
-		} else {
-			log.Println("[E] ", err.Error())
-			//fmt.Fprintln(w, "[E] ", err.Error())
-                }
-	}
+    u, _ := url.Parse(req.URL.String())
+    queryParams := u.Query()
+    wait := queryParams.Get("wait")
+    if len(wait) > 0 {
+        duration, err := time.ParseDuration(wait)
+        if err == nil {
+            fmt.Println("request will be delayed for :", duration)
+            time.Sleep(duration)
+        } else {
+            log.Println("[E] ", err.Error())
+            //fmt.Fprintln(w, "[E] ", err.Error())
+        }
+    }
+    
+    w.Header().Set("Connection", "close")
+    
+    hostname, _ := os.Hostname()
+    fmt.Fprintln(w, "Hostname:", hostname)
+    fmt.Fprintln(w, "\n---- Http Request Headers ----\n")
+    req.Write(w)
 
-	hostname, _ := os.Hostname()
-	fmt.Fprintln(w, "Hostname:", hostname)
-	fmt.Fprintln(w, "\n---- Http Request Headers ----\n")
-	req.Write(w)
-
-	fmt.Fprintln(w, "\n---- Active Endpoint ----\n")
-	fmt.Fprintln(w, "[howto] version: 0.6 \n",
-                        "   curl 127.0.0.1/ \n",
-                        "   curl 127.0.0.1/?wait=2s \n",
-                        "   curl 127.0.0.1/test \n",
-                        "   curl 127.0.0.1/api \n",
-                        "   curl 127.0.0.1/health \n",
-                        "   curl 127.0.0.1/health -d '302' \n",
-                        "\n")
+    fmt.Fprintln(w, "\n---- Active Endpoint ----\n")
+    fmt.Fprintln(w, "[howto] version: 0.7 \n",
+                    "   curl 127.0.0.1/ \n",
+                    "   curl 127.0.0.1/?wait=2s \n",
+                    "   curl 127.0.0.1/test \n",
+                    "   curl 127.0.0.1/api \n",
+                    "   curl 127.0.0.1/health \n",
+                    "   curl 127.0.0.1/health -d '302' \n",
+                    "\n")
 }
 
 
@@ -102,16 +103,16 @@ func index(w http.ResponseWriter, req *http.Request) {
 */
 
 func api(w http.ResponseWriter, req *http.Request) {
-	hostname, _ := os.Hostname()
-	data := struct {
-		Hostname string      `json:"hostname,omitempty"`
-		Headers  http.Header `json:"headers,omitempty"`
-	}{
-		hostname,
-		req.Header,
-	}
+    hostname, _ := os.Hostname()
+    data := struct {
+        Hostname string      `json:"hostname,omitempty"`
+        Headers  http.Header `json:"headers,omitempty"`
+    }{
+        hostname,
+        req.Header,
+    }
 
-	json.NewEncoder(w).Encode(data)
+    json.NewEncoder(w).Encode(data)
 }
 
 
@@ -122,34 +123,34 @@ func api(w http.ResponseWriter, req *http.Request) {
 */
 
 type healthState struct {
-	StatusCode int
+    StatusCode int
 }
 
 var currentHealthState = healthState{200}
 var mutexHealthState = &sync.RWMutex{}
 
 func healthHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		var statusCode int
-		err := json.NewDecoder(req.Body).Decode(&statusCode)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(err.Error()))
-			log.Println(err.Error())
-		} else {
-			mutexHealthState.Lock()
-			defer mutexHealthState.Unlock()
+    if req.Method == http.MethodPost {
+        var statusCode int
+        err := json.NewDecoder(req.Body).Decode(&statusCode)
+        if err != nil {
+            w.WriteHeader(http.StatusBadRequest)
+            w.Write([]byte(err.Error()))
+            log.Println(err.Error())
+        } else {
+            mutexHealthState.Lock()
+            defer mutexHealthState.Unlock()
 
-			currentHealthState.StatusCode = statusCode
-			log.Println("Update 'health check' status code =", statusCode)
-			fmt.Fprintln(w, statusCode)
-		}
-	} else {
-		mutexHealthState.RLock()
-		defer mutexHealthState.RUnlock()
+            currentHealthState.StatusCode = statusCode
+            fmt.Println("Update 'health check' status code =", statusCode)
+            fmt.Fprintln(w, statusCode)
+        }
+    } else {
+        mutexHealthState.RLock()
+        defer mutexHealthState.RUnlock()
 
-		w.WriteHeader(currentHealthState.StatusCode)
-		log.Println("Current 'health check' status code =", currentHealthState.StatusCode)
-	        fmt.Fprintln(w, currentHealthState.StatusCode)
-	}
+        w.WriteHeader(currentHealthState.StatusCode)
+        fmt.Println("Current 'health check' status code =", currentHealthState.StatusCode)
+        fmt.Fprintln(w, currentHealthState.StatusCode)
+    }
 }
