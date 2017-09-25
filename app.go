d4f36e11bd3597a53b5be84d9339968e2a5de291
opera443399 <pc@pcswo.com>
2017-09-25 15:03:36 +0800

    doc(whoami): testing
    try:
    push image:version
    push image:latest
    
    but:
    latest != version
    
    why?

--- app.go
+++ app.go
@@ -6,7 +6,7 @@
  * go install app.go
  * CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o whoamI
  *
- * pc@2017/9/4
+ * pc@2017/9/25
  *
 */
 
@@ -86,7 +86,7 @@ func index(w http.ResponseWriter, req *http.Request) {
     req.Write(w)
 
     fmt.Fprintln(w, "\n---- Active Endpoint ----\n")
-    fmt.Fprintln(w, "[howto] version: 0.8 \n",
+    fmt.Fprintln(w, "[howto] version: 0.9 \n",
                     "   curl 127.0.0.1/ \n",
                     "   curl 127.0.0.1/?wait=2s \n",
                     "   curl 127.0.0.1/test \n",
